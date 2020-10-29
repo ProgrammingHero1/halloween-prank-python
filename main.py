ff3a65e2d35e87734b411742d9c33b6418f55ded
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2020-10-29 08:22:20 -0500

    halloween prank using python. python project.

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('ratsasan.mp3')
+pygame.mixer.music.play()
+sleep(5)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('scr.jpg')
+window.blit(image, (0,0))
+pygame.display.update()
+sleep(3)
